c948a7837a2886ccbed5b6f9c113cf8c5d32f0de
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-09-11 23:27:19 +0800

    set up auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-import "./globals.css";
+import "../globals.css";
 import { ToasterProvider } from "@/lib/ToasterProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,10 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClerkProvider>
-          <ToasterProvider />
           {children}
-        </ClerkProvider>
       </body>
     </html>
   );
