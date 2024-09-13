ad94d01941defa9a77c5a3b50a399308b101e393
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2024-09-13 10:42:27 +0800

    update category ui

--- components/category.tsx
+++ components/category.tsx
@@ -49,13 +49,13 @@ const CategorySection = () => {
         </div>
 
         {/* Grid Section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-8">
           {categories.map((category) => (
             <div
               key={category.name}
               className="flex flex-col gap-4 hover:opacity-75 cursor-pointer"
             >
-              <div className="bg-muted rounded-md aspect-video mb-4 relative">
+              <div className="bg-muted rounded-md aspect-square mb-4 relative">
                 <Image
                   src={category.imageSrc}
                   alt={category.imageAlt}
